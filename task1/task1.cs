5cd7f02326dfae5cb2223443515cb203420bac58
Tatiana Yakovleva <mellyorchid@gmail.com>
2022-10-25 18:19:32 +0300

    first commit

---
+++ task1/task1.cs
@@ -0,0 +1,27 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+
+int [] GetArray(int size){
+    int [] res = new int[size];
+    for (int i = 0; i < size; i++){
+        res[i] = int.Parse(Console.ReadLine());
+    }
+    return res;
+}
+
+Console.WriteLine("Введите количество чисел: ");
+int size = int.Parse(Console.ReadLine());
+int [] array = GetArray(size);
+Console.WriteLine(Count());
+
+int Count(){
+    int count = 0;
+    foreach (int el in array){
+        if (el > 0) count++;
+    }
+    return count;
+}
---
+++ task2/task2.cs
@@ -0,0 +1,22 @@
+﻿/*  Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)  */
+
+
+double Parameters(){
+    Console.Write("Введите параметр: ");
+    double parameters = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    return parameters;
+}
+
+
+void Intersection(double k1, double b1, double k2, double b2){
+    double x = ((b2-b1)/(k1-k2));
+    double y = (k2 * ((b2-b1)/(k1-k2)) + b2);
+    Console.WriteLine(x + " " + y);
+}
+
+Intersection(Parameters(), Parameters(), Parameters(), Parameters());
